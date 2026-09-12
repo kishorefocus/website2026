f935c6f9c2284e2498802008dfc83e99b919091d
kishore <kishoretest42@gmail.com>
2026-09-12 09:34:12 +0530

    jj

--- frontend/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app\\\\page.tsx -> @/components/marketing/interactive-map\":{\"id\":\"app\\\\page.tsx -> @/components/marketing/interactive-map\",\"files\":[\"static/css/_app-pages-browser_components_marketing_interactive-map_tsx.css\",\"static/chunks/_app-pages-browser_components_marketing_interactive-map_tsx.js\"]}}"
